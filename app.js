b17164d894508c8e0ee9c9da77a8490b3c29384f
niirdan <121875046+niirdan@users.noreply.github.com>
2023-01-04 01:57:10 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '123456',
+    database : 'dan',
+    multipleStatements: true
+  });
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('connected as id ' + connection.threadId);
+  });
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.static(__dirname + "/public"))
+
+app.get("/", function(req, res){
+    // Find count of users in DB
+    let search = req.query.search
+    var q= `select * from patient`
+    if (req.query.search) {
+      var q= `SELECT *  
+      FROM patient  
+      WHERE FirstName LIKE '%${req.query.search}%' or ID LIKE '%${req.query.search}%' or LastName LIKE '%${req.query.search}%'`
+    }
+
+    connection.query(q, function(err, results){
+        if(err) throw err;
+        const qer = results 
+        res.render("index", {search,qer});
+    });
+});
+
+
+app.listen('3000',()=> {
+    console.log('start 3000')
+})
